eb210128ba08926dec79778c954d81d26125e5aa
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-12-19 20:52:58 -0300

    Changes to make the code more readable

--- ble.c
+++ ble.c
@@ -14,6 +14,14 @@
 
 
 // Prototype declaration -------------------------------------------------------------------------------------
+void verify_if_the_bluetooth_can_be_used();
+void set_scan_parameters();
+void start_scan();
+void fetch_hci_filter();
+void clear_filter();
+void set_event_filter();
+void set_packet_type_filter();
+void apply_filter();
 void handle_advertising_data();
 
 
@@ -27,75 +35,32 @@ const char *YELLOW = "\033[33m";
 
 int main() {
     int dev_id, sock, len;
-    uint8_t buf[HCI_MAX_EVENT_SIZE];
+    uint8_t buffer[HCI_MAX_EVENT_SIZE];
     struct hci_filter original_filter, new_filter;
 
     dev_id = hci_get_route(NULL);
-    sock = hci_open_dev(dev_id);
+    sock   = hci_open_dev(dev_id);
+    
+    verify_if_the_bluetooth_can_be_used(&dev_id, &sock);
+    set_scan_parameters(&sock);
+    start_scan(&sock);
+    fetch_hci_filter(&sock, &original_filter);
+    clear_filter(&new_filter);
+    set_event_filter(&new_filter);
+    set_packet_type_filter(&new_filter);
+    apply_filter(&sock, &new_filter);
 
-    if (dev_id < 0 || sock < 0) {
-        perror("Failed to open Bluetooth device");
-        exit(1);
-    }
-
-    struct hci_request scan_req;
-    le_set_scan_parameters_cp scan_params = { 0x01, htobs(0x10), htobs(0x10), 0x00, 0x00 };
-    memset(&scan_req, 0, sizeof(scan_req));
-    scan_req.ogf = OGF_LE_CTL;
-    scan_req.ocf = OCF_LE_SET_SCAN_PARAMETERS;
-    scan_req.cparam = &scan_params;
-    scan_req.clen = sizeof(scan_params);
-    scan_req.rparam = NULL;
-    scan_req.rlen = 0;
-
-    if (hci_send_req(sock, &scan_req, 1000) < 0) {
-        perror("Failed to set scan parameters");
-        close(sock);
-        exit(1);
-    }
-
-    le_set_scan_enable_cp scan_enable_cp = { 0x01, 0x00 };
-
-    struct hci_request enable_req;
-    memset(&enable_req, 0, sizeof(enable_req));
-    enable_req.ogf = OGF_LE_CTL;
-    enable_req.ocf = OCF_LE_SET_SCAN_ENABLE;
-    enable_req.cparam = &scan_enable_cp;
-    enable_req.clen = sizeof(scan_enable_cp);
-
-    if (hci_send_req(sock, &enable_req, 1000) < 0) {
-        perror("Failed to start scanning");
-        close(sock);
-        exit(1);
-    }
-
-    socklen_t original_filter_len = sizeof(original_filter);
-    if (getsockopt(sock, SOL_HCI, HCI_FILTER, &original_filter, &original_filter_len) < 0) {
-        perror("Could not get original HCI filter");
-        close(sock);
-        exit(1);
-    }
-
-    hci_filter_clear(&new_filter);
-    hci_filter_set_ptype(HCI_EVENT_PKT, &new_filter);
-    hci_filter_set_event(EVT_LE_META_EVENT, &new_filter);
-
-    if (setsockopt(sock, SOL_HCI, HCI_FILTER, &new_filter, sizeof(new_filter)) < 0) {
-        perror("Could not set HCI filter");
-        close(sock);
-        exit(1);
-    }
 
     printf("Scanning BLE devices continuously...\n");
 
     while (1) {
-        len = read(sock, buf, sizeof(buf));
+        len = read(sock, buffer, sizeof(buffer));
         if (len < 0) {
             perror("Error reading HCI events");
             break;
         }
 
-        evt_le_meta_event *meta_event = (evt_le_meta_event *)(buf + (1 + HCI_EVENT_HDR_SIZE));
+        evt_le_meta_event *meta_event = (evt_le_meta_event *)(buffer + (1 + HCI_EVENT_HDR_SIZE));
         if (meta_event->subevent != EVT_LE_ADVERTISING_REPORT)
             continue;
 
@@ -113,6 +78,90 @@ int main() {
     close(sock);
     return 0;
 }
+
+
+
+void verify_if_the_bluetooth_can_be_used(int *dev_id, int *sock) {
+    if (*dev_id < 0 || *sock < 0){
+        perror("Failed to open Bluetooth device");
+        exit(1);
+    }
+}
+
+
+
+void set_scan_parameters(int *sock) {
+    struct hci_request scan_req;
+    le_set_scan_parameters_cp scan_params = { 0x01, htobs(0x10), htobs(0x10), 0x00, 0x00 };
+    memset(&scan_req, 0, sizeof(scan_req));
+    scan_req.ogf    = OGF_LE_CTL;
+    scan_req.ocf    = OCF_LE_SET_SCAN_PARAMETERS;
+    scan_req.cparam = &scan_params;
+    scan_req.clen   = sizeof(scan_params);
+    scan_req.rparam = NULL;
+    scan_req.rlen   = 0;
+
+    if (hci_send_req(*sock, &scan_req, 1000) < 0) {
+        perror("Failed to set scan parameters");
+        close(*sock);
+        exit(1);
+    }
+}
+
+
+
+void start_scan(int *sock){
+    le_set_scan_enable_cp scan_enable_cp = { 0x01, 0x00 };
+
+    struct hci_request enable_req;
+    memset(&enable_req, 0, sizeof(enable_req));
+    enable_req.ogf    = OGF_LE_CTL;
+    enable_req.ocf    = OCF_LE_SET_SCAN_ENABLE;
+    enable_req.cparam = &scan_enable_cp;
+    enable_req.clen   = sizeof(scan_enable_cp);
+
+    if (hci_send_req(*sock, &enable_req, 1000) < 0) {
+        perror("Failed to start scanning");
+        close(*sock);
+        exit(1);
+    }
+}
+
+
+
+void fetch_hci_filter(int *sock, struct hci_filter *original_filter) {
+    socklen_t original_filter_len = sizeof(*original_filter);
+    if (getsockopt(*sock, SOL_HCI, HCI_FILTER, original_filter, &original_filter_len) < 0) {
+        perror("Could not get original HCI filter");
+        close(*sock);
+        exit(1);
+    }
+}
+
+
+
+void clear_filter(struct hci_filter *filter) {
+    hci_filter_clear(filter);
+}
+
+void set_event_filter(struct hci_filter *filter) {
+    hci_filter_set_event(EVT_LE_META_EVENT, filter);
+}
+
+void set_packet_type_filter(struct hci_filter *filter) {
+    hci_filter_set_ptype(HCI_EVENT_PKT, filter);
+}
+
+
+
+void apply_filter(int *sock, struct hci_filter *filter) {
+    if (setsockopt(*sock, SOL_HCI, HCI_FILTER, filter, sizeof(*filter)) < 0) {
+        perror("Could not set HCI filter");
+        close(*sock);
+        exit(1);
+    }
+}
+
 
 
 void handle_advertising_data(uint8_t *data, int length) {
