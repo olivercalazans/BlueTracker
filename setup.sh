7540e81b79148a9c55441c6166873e4b0196e1b7
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-01-08 09:01:41 -0300

    Changes to make the script download the dependencies

--- setup.sh
+++ setup.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 
-FILE="bluetracker"
-DIR="$HOME/.bluetracker"
 
-# Find the path of the ble.c file
-PATH_C=$(find "$HOME" -name ble.c -exec dirname {} \; 2>/dev/null)
+# Ensure the script is executed with root privileges
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root. Please use 'sudo' to execute it."
+  exec sudo "$0" "$@"
+fi
+
+
+HOME_DIR=$(eval echo "~$SUDO_USER")
+DIR="$HOME_DIR/.bluetracker"
+FILE="bluetracker"
+PATH_C=$(dirname "$(realpath "$0")")
+
 
 # Check if the ble.c file was found
-if [[ -z $PATH_C ]]; then
+if [[ ! -e "$PATH_C/ble.c" ]]; then
     echo "Error: The 'ble.c' file was not found in the \$HOME directory."
     exit 1
 fi
 
+
+if ! sudo apt install build-essential libbluetooth-dev > /dev/null 2>&1; then
+    echo "Error: it was not possible to install the dependencies"
+fi
+
+
 # Create the initialization script
 echo "Creating the initialization script..."
 echo "#!/bin/bash" > $FILE
-echo "sudo $DIR/bluetracker.out \"\$@\"" >> $FILE
+echo "if [ \"\$EUID\" -ne 0 ]; then" >> $FILE
+echo "  exec sudo \"\$0\" \"\$@\"" >> $FILE
+echo "fi" >> $FILE
+echo "HOME_DIR=\$(eval echo "~\$SUDO_USER")" >> $FILE
+echo "sudo \$HOME_DIR/bluetracker.out \"\$@\"" >> $FILE
+
 
 # Move the script to /usr/bin
 echo "Moving the script to /usr/bin..."
 sudo mv $FILE /usr/bin/
 sudo chmod +x /usr/bin/$FILE
 
+
 # Create the program directory
 echo "Creating the directory $DIR..."
 mkdir -p "$DIR"
+
 
 # Copy the LICENSE file if it exists
 if [[ -f "$PATH_C/LICENSE" ]]; then
@@ -34,9 +55,11 @@ else
     echo "Warning: The LICENSE file was not found."
 fi
 
+
 # Compile the code
 echo "Compiling the code..."
 gcc "$PATH_C/ble.c" -o "$DIR/bluetracker.out" -lbluetooth -lm
+
 
 # Check if compilation was successful
 if [[ $? -ne 0 ]]; then
